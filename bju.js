0f07d528a5a5e928f9c897aff943a2146afd9850
WavePlayz <34413913+WavePlayz@users.noreply.github.com>
2022-05-26 16:32:59 +0530

    update bju.js

--- bju.js
+++ bju.js
@@ -1,1 +1,29 @@
 
+
+const ElementTypes = [ "label", "image", "panel" ]
+
+class Element {
+	constructor () {
+		
+	}
+}
+
+class Label {
+	constructor (text) {
+		this.type = ElementTypes[0]
+		this.text = text
+	}
+}
+
+
+
+class BJU {
+	constructor(namespace) {
+		this.namespace = namespace
+	}
+	
+	addElement (name, element) {
+		this[name] = element
+	}
+}
+
